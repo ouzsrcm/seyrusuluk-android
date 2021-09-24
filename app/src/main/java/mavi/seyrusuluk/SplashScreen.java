52a04a6fcff597abd311a5716fe441565d422773
ouzsrcm <oguzhan.saricam@icloud.com>
2021-09-24 17:52:32 +0300

    initial

---
+++ app/src/main/java/mavi/seyrusuluk/SplashScreen.java
@@ -0,0 +1,16 @@
+package mavi.seyrusuluk;
+
+import mavi.seyrusuluk.util.SystemUiHider;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+}
---
+++ app/src/main/java/mavi/seyrusuluk/dummy/DummyContent.java
@@ -0,0 +1,74 @@
+package mavi.seyrusuluk.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    static {
+        // Add 3 sample items.
+        addItem(new DummyItem("1", "Takdim", "“İşte \tbu \tbenim \tsırat-ı \n" +
+                "müstakîmimdir, buna uyun. Başka yollara sapmayın, sonra onlar sizi Allah’ın yolundan ayırırlar.” \n" +
+                "‘Şeriat, tarikat, marifet ve hakikat’ temellerinden oluşan İslam’ın mânevî dinamiklerini, bir merkezi çevreleyen daireler şeklinde düşündüğümüzde, Seyr ü Sülûk’u, bu merkeze doğru gerçekleştirilen bir sefer olarak tanımlayabiliriz. Mürşid-i Kâmil ise bu mânevî yolculuğu tamamlayarak geriye dönen ve kendisine tâbi olanları aynı yollardan geçirmeye çalışan, Hakk’a ulaştıran kişidir. Mürşid-i Kâmil iyi bir tabîb, iyi bir rehberdir.  \n" +
+                "Kendisi bu yollardan geçmiş, karşılaşılabilecek muhtemel tehlikeleri görmüş, onların üstesinden gelmiş olduğu için bir sâlikin de hangi noktalarda sıkıntıya düşebileceğini çok iyi bilmektedir.  \n" +
+                " \n" +
+                "Mü’minin; nefs, şeytan ve dünya ile olan imtihanında nefsinin yeterli olgunluğa ermemiş olması; onun helal ile haram, doğru ile yanlış arasında yapacağı tercihlerde kendi aleyhine olanı öncelemesi gibi olumsuz bir davranışa yol açabilir. ‘Biz Allah’tan geldik ve tekrar O’na döneceğiz’ gerçeğine binaen bu dönüş seyrini tam bir bilinç ve hakiki bir hâle dönüştürmek için Seyr ü Sülûk denilen bu -kimi yerlerinde dar, kimi yerlerinde uçurumlu, kimi yerlerinde karanlıklarla dolu ama nihayetinde korkulardan salim, ölmeden evvel, daha dünyada iken müjdeleri tattıran- yolun tamamlanması gerekmektedir.  \n" +
+                "Karınca misali, bu yola çıkmaya niyet etmek, azmetmek, vuslat gerçekleşmese dâhi bu gayret sürerken Hakk’a yürümüş olmak îcap etmektedir.  \n" +
+                "Yayınlamış olduğumuz bu kitap daha ziyade rûhun eğitimini gözeten bir bakış açısıyla yazılmıştır.  \n" +
+                "Yazarımızın çeşitli vesilelerle vurgulamış olduğu üzere tasavvuf ehli, nefsin eğitiminden ziyade rûhun eğitimine önem vermiştir. Ruh, kemâl bulduğu zaman nefs de yeterli seviyeye gelecek demektir.  \n" +
+                "Rûhun kemâlini öne almak, nefsi geri planda görüp onu ihmal etmek manasında anlaşılmamalıdır. \n" +
+                "Asr-ı Saadet’i, Müslümanların altın çağı kılan temel vasıf, elbette, onların mânevî zenginlikleridir. Bütün bir Ashab içinde, birbirine aktarılan ve bir diğerini kendine dönüştüren bu yapı, Hakk ile ve Hakk’ın Habîbi (sav) ile sürekli yüzyüze olma hâlinden ileri gelmektedir.  \n" +
+                "Bütün Müslümanlar olarak o ‘Altın Çağ’ benzeri bir âşıklar topluluğu kurma isteği ile yaşayıp durmuşuzdur. O seviyeye ulaşılamadığı için de bu, içimizde hep bir ukde olarak kalmıştır.  \n" +
+                "Elinizde bulunan bu kitap dahi, Seyr ü Sülûk’unu tamamlayıp birbirine ‘Hakk’ı ve sabrı tavsiye eden’, aynı zamanda birbirini dönüştürme işlevini gören ‘Hakk Yolcularını’ yetiştirme ve sâdıklar topluluğu oluşturma gayesine katkıda bulunma amacına binâen yazılmıştır. Aşağıda sunulan alıntının göstermiş olduğu hedefleri bu kitabın gözetmiş olduğu gaye olarak da alabiliriz: \n" +
+                "Abdullah İbn Mes’ud el-Hüzelî (ra)’nin anlattığına göre, bir adam kendisine, \n" +
+                "“Sırat-ı müstakîm (doğru yol) nedir? diye sordu. İbn Mesud ise ona şu cevabı verdi:  \n" +
+                "“Hz. Muhammed (sav), bizi sırat-ı müstakîmin bir başında bıraktı. Bu yolun öbür ucu ise Cennet’e ulaşmaktadır. Bu ana yolun sağında ve solunda başka tâlî yollar da vardır. Bunlardan her birinin başında bir kısım insanlar durmuş, oradan geçenleri kendilerine çağırıyorlar. Kim bu dış yollardan birine sülûk ederse, yol onu ateşe götürecektir. Kim de Sırat-ı Müstakîme sülûk ederse o da Cennet’e ulaşacaktır.”  \n" +
+                "İbn Mes’ud bu açıklamayı yaptıktan sonra şu âyeti okudu: \n" +
+                "“İşte bu benim sırat-ı müstakîmimdir, buna uyun. Başka yollara sapmayın, sonra onlar sizi Allah’ın yolundan ayırırlar.”  \n" +
+                "Bu yolun yolcuları için, kısa ama özlü bir içerikle hazırlanan bu eserin tesirlerinin âzami ölçüde olmasını Rabbimizden niyaz ederiz. Üstadımıza, özellikle böyle bir amaç için hazırlamış olduğu bu güzîde çalışmasından dolayı da teşekkürlerimizi arzederiz.  \n" +
+                "Gayret bizden tevfik Allah’tandır.  \n"));
+    }
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class DummyItem {
+        public String id;
+        public String content;
+        public String description;
+
+        public DummyItem(String id, String content, String description) {
+            this.id = id;
+            this.content = content;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
